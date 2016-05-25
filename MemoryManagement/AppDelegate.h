6c4b1d61ec01efed71ff9ef02f63bf90efec420a
NatureZhang <895234387@qq.com>
2016-05-25 10:37:48 +0800

    Initial Commit

---
+++ MemoryManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryManagement
+//
+//  Created by zhangdong on 16/5/25.
+//  Copyright © 2016年 __Nature__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryManagement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryManagement
+//
+//  Created by zhangdong on 16/5/25.
+//  Copyright © 2016年 __Nature__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
